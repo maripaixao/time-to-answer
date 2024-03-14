bb7437b36d26425553c6e1fd8b8646af3dbcbd5f
Marina <marinapaixao_@hotmail.com>
2024-03-13 22:12:22 -0300

    Showing total questions by subject

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,12 @@
 module SiteHelper
+  def msg_jumbotron
+    case params[:action]
+    when 'index'
+      "Últimas perguntas cadastradas..."
+    when 'questions'
+      "Resultados para o termo \"#{params[:term]}\"..."
+    when 'subject'
+      "Exibindo todos os resultados de \"#{params[:subject]}\"..."
+    end
+  end
 end
